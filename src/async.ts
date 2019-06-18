50afbabc6610e566f21757dc4d031a26cb041374
hsjakobsen <hsjakobsen@gmail.com>
2019-06-18 14:13:40 +0200

    converted loop to for... ... of

--- src/async.ts
+++ src/async.ts
@@ -1,5 +1,5 @@
 export const asyncForEach = async (array:any[], callback: (item:object) => void) => {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index]);
+  for (var item of array) {
+    await callback(item);
   }
 }
