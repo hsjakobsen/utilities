3c74250520d3ca2df1e051638042e26fa4b45b73
hsjakobsen <hsjakobsen@gmail.com>
2019-06-24 15:13:31 +0200

    1.0.16

--- src/index.ts
+++ src/index.ts
@@ -27,9 +27,9 @@ export const stringifyAndParse = (item: any) => utilsParse.stringifyAndParse(ite
 export const getBrowserInfo = (userAgent: string, appName: string, appVersion: string) =>
   utilsBrowser.getBrowserInfo(userAgent, appName, appVersion);
 
-export const formatResponse = (message:string, success:boolean, data?:any, extraData?:any) =>
+export const formatResponse = (message: string, success: boolean, data?: any, extraData?: any) =>
   utilsResponse.formatResponse(message, success, data, extraData);
-export const formatError = (error:string) => utilsResponse.formatError(error);
+export const formatError = (error: string) => utilsResponse.formatError(error);
 
 export const validateEmail = (email: string) => utilsValidation.validateEmail(email);
 export const trimStringAndCheckLength = (
