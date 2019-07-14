c4da7aa1746764e6aaef8c80503430ad14b353f5
hsjakobsen <hsjakobsen@gmail.com>
2019-07-14 21:58:55 +0200

    1.0.19

--- src/date.ts
+++ src/date.ts
@@ -7,16 +7,16 @@ export const timeFormatHMS: string = 'HH:mm:ss';
 
 export const monthAndDateFormatted = (date: Date) => {
   return moment(date).format(dateFormatMD);
-}
+};
 
 export const getTodayDateFormatted = () => {
   return moment(new Date()).format(dateFormatDMY);
-}
+};
 
 export const getTimestamp = (date: Date) => {
   return moment(date).format(timeFormatHMS);
-}
+};
 
 export const getSignUpDateForDatabase = () => {
-  return moment(new Date()).format(dateFormatYMD + " " + timeFormatHMS);
-}
+  return moment(new Date()).format(dateFormatYMD + ' ' + timeFormatHMS);
+};
