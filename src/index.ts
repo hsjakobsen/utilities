677124d70b8511319d900ec02f728666c586d775
hsjakobsen <hsjakobsen@gmail.com>
2020-05-01 14:18:22 +0200

    Updated utilities to fix vulnerabilities Added new formatting function for numbers Cleaned up index file to simplify exports

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,9 @@
-import * as utilsAsync from './async';
-import * as utilsBrowser from './browser';
-import * as utilsComparison from './comparison';
-import * as utilsDate from './date';
-import * as utilsNumber from './numbers';
-import * as utilsParse from './parse';
-import * as utilsResponse from './responses';
-import * as utilsSort from './sort';
-import * as utilsValidation from './validation';
-
-export const asyncForEach = (array: any[], callback: (item: any) => void) => utilsAsync.asyncForEach(array, callback);
-
-export const compareStrings = (string1: string, string2: string) => utilsComparison.compareStrings(string1, string2);
-
-export const getMonthAndDateFormatted = (date: string) => utilsDate.monthAndDateFormatted(date);
-export const getTodayDateFormatted = () => utilsDate.getTodayDateFormatted();
-export const getTimestamp = (date: Date) => utilsDate.getTimestamp(date);
-export const getSignUpDateForDatabase = () => utilsDate.getSignUpDateForDatabase();
-export const dateFormatYMD = utilsDate.dateFormatYMD;
-export const dateFormatDMY = utilsDate.dateFormatDMY;
-export const dateFormatMD = utilsDate.dateFormatMD;
-export const timeFormatHMS = utilsDate.timeFormatHMS;
-export const timeFormatHM = utilsDate.timeFormatHM;
-export const verboseDateFormat = utilsDate.verboseDateFormat;
-
-export const numberWithCommas = (numString: string) => utilsNumber.numberWithCommas(numString);
-
-export const stringifyAndParse = (item: any) => utilsParse.stringifyAndParse(item);
-
-export const getBrowserInfo = (userAgent: string, appName: string, appVersion: string) =>
-  utilsBrowser.getBrowserInfo(userAgent, appName, appVersion);
-
-export const formatResponse = (message: string, success: boolean, data?: any, extraData?: any) =>
-  utilsResponse.formatResponse(message, success, data, extraData);
-export const formatError = (error: string) => utilsResponse.formatError(error);
-
-export const sortByKey = (list: any[], key: string, ascending: boolean) => utilsSort.sortByKey(list, key, ascending);
-
-export const validateEmail = (email: string) => utilsValidation.validateEmail(email);
-export const trimStringAndCheckLength = (
-  stringToCheck: string,
-  field: string,
-  lengthRequirement: number,
-  matchLength: boolean,
-) => utilsValidation.trimStringAndCheckLength(stringToCheck, field, lengthRequirement, matchLength);
-export const verifyLoginCredentials = (username: string, password: string) =>
-  utilsValidation.verifyLoginCredentials(username, password);
+export { asyncForEach } from './async';
+export { getBrowserInfo } from './browser';
+export { compareStrings } from './comparison';
+export { dateFormatDMY, dateFormatMD, dateFormatYMD, getSignUpDateForDatabase, getTimestamp, getTodayDateFormatted, monthAndDateFormatted, timeFormatHM, timeFormatHMS, verboseDateFormat } from './date';
+export { formatNumberReadable, numberWithCommas } from './numbers';
+export { stringifyAndParse } from './parse';
+export { formatError, formatResponse, IResponse } from './responses';
+export { sortByKey } from './sort';
+export { IVerifiedLoginCredentials, trimStringAndCheckLength, validateEmail, verifyLoginCredentials } from './validation';
--- src/numbers.ts
+++ src/numbers.ts
@@ -1,15 +1,9 @@
-// function parseLocaleNumber(stringNumber) {
-//   var stringNumberToProcess = numberWithCommas(stringNumber);
-//   var thousandSeparator = (1111).toLocaleString().replace(/1/g, '');
-//   var decimalSeparator = (1.1).toLocaleString().replace(/1/g, '');
-
-//   return stringNumberToProcess
-//     .replace(new RegExp('\\' + thousandSeparator, 'g'), '')
-//     .replace(new RegExp('\\' + decimalSeparator), '.');
-// }
-
-export function numberWithCommas(x: string) {
+export const numberWithCommas = (x: string) => {
   const parts = x.toString().split('.');
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
+}
+
+export const formatNumberReadable = (numberToFormat:number) => {
+  return numberToFormat.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
