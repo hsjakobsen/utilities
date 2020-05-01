5051ed19e546ee5e6bc0c475a3649ed36e1fd14e
hsjakobsen <hsjakobsen@gmail.com>
2020-05-01 14:18:47 +0200

    2.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 export { asyncForEach } from './async';
 export { getBrowserInfo } from './browser';
 export { compareStrings } from './comparison';
-export { dateFormatDMY, dateFormatMD, dateFormatYMD, getSignUpDateForDatabase, getTimestamp, getTodayDateFormatted, monthAndDateFormatted, timeFormatHM, timeFormatHMS, verboseDateFormat } from './date';
+export {
+  dateFormatDMY,
+  dateFormatMD,
+  dateFormatYMD,
+  getSignUpDateForDatabase,
+  getTimestamp,
+  getTodayDateFormatted,
+  monthAndDateFormatted,
+  timeFormatHM,
+  timeFormatHMS,
+  verboseDateFormat,
+} from './date';
 export { formatNumberReadable, numberWithCommas } from './numbers';
 export { stringifyAndParse } from './parse';
 export { formatError, formatResponse, IResponse } from './responses';
 export { sortByKey } from './sort';
-export { IVerifiedLoginCredentials, trimStringAndCheckLength, validateEmail, verifyLoginCredentials } from './validation';
+export {
+  IVerifiedLoginCredentials,
+  trimStringAndCheckLength,
+  validateEmail,
+  verifyLoginCredentials,
+} from './validation';
--- src/numbers.ts
+++ src/numbers.ts
@@ -2,8 +2,8 @@ export const numberWithCommas = (x: string) => {
   const parts = x.toString().split('.');
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   return parts.join('.');
-}
+};
 
-export const formatNumberReadable = (numberToFormat:number) => {
-  return numberToFormat.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-}
+export const formatNumberReadable = (numberToFormat: number) => {
+  return numberToFormat.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+};
