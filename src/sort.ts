83146302cb8b955eef25c6f13f387c8cdfa4753e
hsjakobsen <hsjakobsen@gmail.com>
2020-06-22 14:04:14 +0200

    Fix to make sorting work

--- src/sort.ts
+++ src/sort.ts
@@ -1,9 +1,11 @@
 export const sortByKey = (array: any[], key: string, ascending: boolean): any[] => {
   return array.sort((a, b) => {
-    const x: string = (a[key] as string).toLowerCase();
-    const y: string = (b[key] as string).toLowerCase();
+    const x: string = a[key] as string;
+    const y: string = b[key] as string;
+    const xLowerCased = x.toLowerCase();
+    const yLowerCased = y.toLowerCase();
 
-    let result: number = x < y ? -1 : x > y ? 1 : 0;
+    let result: number = xLowerCased < yLowerCased ? -1 : xLowerCased > yLowerCased ? 1 : 0;
     if (!ascending) {
       result = result * -1;
     }
