78924df22ef015f3a536399141ebac5ee240560c
hsjakobsen <henrikj@flowplan.dk>
2021-05-27 08:07:00 +0200

    Fixed incorrect data format and updated packages to avoid vulnerabilities

--- src/date.ts
+++ src/date.ts
@@ -5,7 +5,7 @@ export const dateFormatDMY = 'DD-MM-YYYY';
 export const dateFormatMD = 'MM.DD';
 export const timeFormatHMS = 'HH:mm:ss';
 export const timeFormatHM = 'HH:mm';
-export const verboseDateFormat = 'MMMM Do YYYY';
+export const verboseDateFormat = 'MMMM DD YYYY';
 
 export const monthAndDateFormatted = (date: string): string => {
   return dayjs(date).format(dateFormatMD);
